e293d40dac4ed3ab29856b8984a705cf9d6da60f
allenhnn <allenhuang95710@gmail.com>
2024-12-02 03:08:45 +0800

    add frames

---
+++ Frame/src/frames/MainFrame.jsx
@@ -0,0 +1,13 @@
+import '../sass/output.css'
+const MainFrame = () =>{
+    return(
+        <div className="mainFrame">
+            <div className="navSide"></div>
+            <div className="mainSide"></div>
+            <div className="infoSide"></div>
+        </div>
+    )
+    // npm start
+    // sass xx.scss output.css
+}
+export default MainFrame;
---
+++ Frame/src/frames/YoutubeFrame.jsx
@@ -0,0 +1,8 @@
+const YoutubeFrame = () =>{
+    return(
+        <div className="YoutubeFrame">
+            <h1>測試YT FRAME</h1>
+        </div>
+    )
+}
+export default YoutubeFrame;
