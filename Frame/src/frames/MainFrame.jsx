555162f774fa91eeba1f23e05c9a9a2611019f8d
allenhnn <allenhuang95710@gmail.com>
2024-12-02 14:54:14 +0800

    update frame

--- Frame/src/frames/MainFrame.jsx
+++ Frame/src/frames/MainFrame.jsx
@@ -1,10 +1,35 @@
 import '../sass/output.css'
-const MainFrame = () =>{
-    return(
+const MainFrame = () => {
+    const loginStatus = 1;
+    const userName = "Allenhnn_";
+    return (
         <div className="mainFrame">
-            <div className="navSide"></div>
-            <div className="mainSide"></div>
-            <div className="infoSide"></div>
+            <nav id='FrameNavbar'>
+                <h1 className='logo'>PigSystem</h1>
+                <div className='searchBar'>
+                    <div className="home"></div>
+                    <input type="text" />
+                </div>
+                {loginStatus == 0 ?
+                    // 登入
+                    <div className='memberBar'>
+                        <div className="memberImg"></div>
+                        <h3>{userName}</h3>
+                    </div>
+                    :
+                    // 未登入
+                    <div className='memberBar'>
+                        <div className='loginBtn'>註冊</div>
+                        <div className='loginBtn'>登入</div>
+                    </div>
+                }
+
+            </nav>
+            <div className='container'>
+                <div className="navSide"></div>
+                <div className="mainSide"></div>
+                <div className="infoSide"></div>
+            </div>
         </div>
     )
     // npm start
