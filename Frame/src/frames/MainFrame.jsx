e4201f48a5c9f33c2793abdcf515c50689036ca6
allenhnn <allenhuang95710@gmail.com>
2024-12-03 23:25:23 +0800

    update frame

--- Frame/src/frames/MainFrame.jsx
+++ Frame/src/frames/MainFrame.jsx
@@ -1,30 +1,38 @@
+import { useState } from 'react';
 import '../sass/output.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faPause, faForwardStep, faBackwardStep } from '@fortawesome/free-solid-svg-icons'
+
+
 const MainFrame = () => {
-    const loginStatus = 1;
+    const [loginStatus, setLoginStatus] = useState(0);
+
     const userName = "Allenhnn_";
     return (
         <div className="mainFrame">
             <nav id='FrameNavbar'>
                 <h1 className='logo'>PigSystem</h1>
                 <div className='searchBar'>
-                    <div className="home"></div>
+                    <div className="home"><FontAwesomeIcon icon={faHouse} /></div>
                     <input type="text" />
                 </div>
                 {loginStatus == 0 ?
-                    // 登入
-                    <div className='memberBar'>
-                        <div className="memberImg"></div>
-                        <h3>{userName}</h3>
-                    </div>
-                    :
                     // 未登入
                     <div className='memberBar'>
                         <div className='loginBtn'>註冊</div>
                         <div className='loginBtn'>登入</div>
                     </div>
+                    :
+                    // 登入
+                    <div className='memberBar'>
+                        <div className="memberImg"></div>
+                        <h3>{userName}</h3>
+                    </div>
                 }
 
             </nav>
+
             <div className='container'>
                 <div className="navSide"></div>
                 <div className="mainSide">
@@ -90,6 +98,30 @@ const MainFrame = () => {
                 </div>
                 <div className="infoSide"></div>
             </div>
+
+            <div className="playBar">
+                <div className="songBar">
+                    <div className="songImg"></div>
+                    <div className='texts'>
+                        <h3>SongNameName</h3>
+                        <h4>Singer</h4>
+                    </div>
+                </div>
+                <div className="control_container">
+                    <div className="controls">
+                        <div className='icon'><FontAwesomeIcon icon={faBackwardStep} /></div>
+                        <div className='icon'><FontAwesomeIcon icon={faPause} /></div>
+                        <div className='icon'><FontAwesomeIcon icon={faForwardStep} /></div>
+                    </div>
+                    <div className="progression">
+                        <div className="progression_line" />
+                    </div>
+                </div>
+                <div className="volumn">
+
+                </div>
+            </div>
+
         </div>
     )
     // npm start
