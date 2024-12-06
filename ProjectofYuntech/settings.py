1ca41e7e15c27dd100caa9b0feebed150f5b76f7
Dhung-2006 <dexterplayer2007@gmail.com>
2024-12-06 13:27:31 +0800

    user create

--- ProjectofYuntech/settings.py
+++ ProjectofYuntech/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'ProjectofYuntech.urls'
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProjectofYuntech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import User
+from django.http import HttpResponse
 
-# Create your views here.
+def add_user(request):
+    if request.method == "POST":
+        print(request.POST)
+        if request.POST.get('registBtn'):
+            Username = request.POST.get("RegistUsername")
+            Account = request.POST.get("RegistAccount")
+            print(Account)
+            Password = request.POST.get("RegistPassword")
+            email = request.POST.get("RegistEmail")
+            add_user = User.objects.create(
+                user_Name = Username,
+                user_Email = email,
+                user_Account = Account,
+                user_Password = Password
+            )
+            add_user.save()
+   
+        return HttpResponse("correct" , status ="200")
+
+def index(request):
+    return render(request , "test.html" , locals())
